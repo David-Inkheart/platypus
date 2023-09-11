86c0780dab58b68cb239cf46e86945a1383abaee
David-Inkheart <okoliedave1@gmail.com>
2023-09-11 19:56:24 +0100

    restructure folder

--- rgt-Server-Template/src/types.ts
+++
@@ -1,8 +0,0 @@
-import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
-import { Request, Response } from "express";
-
-export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request & { session: Request["session"] & { userId?: number } };
-  res: Response;
-};
